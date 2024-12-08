4423b1bc95e51be00e6773031fa99f7f27f66cb1
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2024-12-08 01:04:48 -0600

    added basic interface to candidateInterface.

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,14 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+
+
+
+export interface CandidateInterface {
+    id: string, // unique identifier
+    name: string, // candidate's name
+    avatar_url: string, // URL to the candidate's avatar
+    bio: string, // Candidate's bio
+    location: string, // Candidate's location
+    public_repos: string, // # of public repos
+
+}
