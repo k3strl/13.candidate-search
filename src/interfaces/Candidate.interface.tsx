55259aa024fefcee53445db568ff21860bb00536
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2024-12-08 10:23:55 -0600

    wrote candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,14 +1,14 @@
-// TODO: Create an interface for the Candidate objects returned by the API
 import { createContext } from 'react';
 
+// DONE?: Create an interface for the Candidate objects returned by the API
 
 export interface CandidateInterface {
-    id: string, // unique identifier
-    name: string, // candidate's name
-    avatar_url: string, // URL to the candidate's avatar
-    bio: string, // Candidate's bio
-    location: string, // Candidate's location
-    public_repos: string, // # of public repos
+    id?: string; // unique identifier
+    name?: string; // candidate's name
+    avatar_url?: string; // URL to the candidate's avatar
+    bio?: string; // Candidate's bio
+    location?: string; // Candidate's location
+    public_repos?: string; // # of public repos
 
 }
 
