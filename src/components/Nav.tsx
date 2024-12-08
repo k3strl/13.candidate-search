2d29308f66861533cc4236c8f5dd6ed3db54020a
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2024-12-08 00:21:51 -0600

    Added navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
+// TODO: Add necessary code to display the navigation bar and link between the pages
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav>
+      <ul>      
+        {/* <ul style={{ display: "flex,", gap: "1rem", listStyle: "none"}}> */}
+      <li>
+        <Link to= "/">Home</Link>
+      </li>
+        <li>
+          <Link to= "/">
+            Potential Candidates
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 };
 
